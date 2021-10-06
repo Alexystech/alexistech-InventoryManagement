3d4753177c123d952b0ddaf38c6e3aa210e179dd
alexis vazquez <definealexis140820@gmail.com>
2021-10-06 17:30:30 -0500

    I've added two jpql instructions in ProductRepository to get specific information

--- src/main/java/com/itsx/alexis/repository/ProductRepository.java
+++ src/main/java/com/itsx/alexis/repository/ProductRepository.java
@@ -1,9 +1,21 @@
 package com.itsx.alexis.repository;
 
 import com.itsx.alexis.entity.Product;
+import com.itsx.alexis.entity.Supplier;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
+
+    @Query(value = "SELECT c FROM Category c WHERE c.idCategory =: idCategory")
+    List<Product> findAllProductsByCategory(@Param("idCategory") long idCategory);
+
+    @Query(value = "SELECT s FROM Suplier s WHERE s.idSupplier =: idSupplier")
+    List<Supplier> findAllProductsBySupplier(@Param("idSupplier") long idSupplier);
+
 }
