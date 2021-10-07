3263c613db93958f936e559f18d1e3ac2961f12a
Alexis <definealexis140820@gmail.com>
2021-10-07 16:26:07 -0500

    I've added swagger ducumentation in Category,Product and supplier Rest Controller

--- src/main/java/com/itsx/alexis/restapi/CategoryRestController.java
+++ src/main/java/com/itsx/alexis/restapi/CategoryRestController.java
@@ -2,6 +2,8 @@ package com.itsx.alexis.restapi;
 
 import com.itsx.alexis.entity.Category;
 import com.itsx.alexis.service.CategoryService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,36 +29,57 @@ public class CategoryRestController {
         this.categoryService = categoryService;
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "category was been created"),
+            @ApiResponse(code = 422, message = "category is null, unprocessable entity")
+    })
     @PostMapping(path = "/create")
     public ResponseEntity<Category> createCategory(@RequestBody Category category) {
         return new ResponseEntity<>(categoryService.createCategory(category)
                 , HttpStatus.CREATED);
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "all categories was been created"),
+    })
     @PostMapping(path = "/create/pull")
     public ResponseEntity<List<Category>> createAPullOfCategories(@RequestBody List<Category> categories) {
         return new ResponseEntity<>(categoryService.createAPullOfCategories(categories)
                 , HttpStatus.CREATED);
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "id accepted, category was been deleted"),
+            @ApiResponse(code = 404, message = "category was not found"),
+            @ApiResponse(code = 422, message = "id out of bound, your id is minor to 1")
+    })
     @DeleteMapping(path = "/delete/{idCategory}")
     public ResponseEntity<Boolean> deleteCategoryById(@PathVariable long idCategory) {
         categoryService.deleteCategoryById(idCategory);
         return new ResponseEntity<>(true, HttpStatus.ACCEPTED);
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "category was updated"),
+            @ApiResponse(code = 422, message = "category is null")
+    })
     @PutMapping(path = "/update")
     public ResponseEntity<Category> updateCategory(@RequestBody Category category) {
         return new ResponseEntity<>(categoryService.updateCategory(category)
                 , HttpStatus.ACCEPTED);
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "category was returned"),
+            @ApiResponse(code = 404, message = "category was not found")
+    })
     @GetMapping(path = "/get/{idCategory}")
     public ResponseEntity<Category> getCategoryById(@PathVariable long idCategory) {
         return new ResponseEntity<>(categoryService.getCategoryById(idCategory)
                 , HttpStatus.OK);
     }
 
+    @ApiResponse(code = 200, message = "all categories was returned")
     @GetMapping(path = "/get/all")
     public ResponseEntity<List<Category>> getAllCategories() {
         return new ResponseEntity<>(categoryService.getAllCategories()
--- src/main/java/com/itsx/alexis/restapi/SupplierRestController.java
+++ src/main/java/com/itsx/alexis/restapi/SupplierRestController.java
@@ -2,6 +2,8 @@ package com.itsx.alexis.restapi;
 
 import com.itsx.alexis.entity.Supplier;
 import com.itsx.alexis.service.SupplierService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,36 +29,57 @@ public class SupplierRestController {
         this.supplierService = supplierService;
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "supplier was been created"),
+            @ApiResponse(code = 422, message = "supplier is null")
+    })
     @PostMapping(path = "/create")
     public ResponseEntity<Supplier> createSupplier(@RequestBody Supplier supplier) {
         return new ResponseEntity<>(supplierService.createSupplier(supplier)
                 , HttpStatus.CREATED);
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "all products was been created"),
+    })
     @PostMapping(path = "/create/pull")
     public ResponseEntity<List<Supplier>> createAPullOfSuppliers(@RequestBody List<Supplier> suppliers) {
         return new ResponseEntity<>(supplierService.createAPullOfSuppliers(suppliers)
                 , HttpStatus.CREATED);
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "id accepted, supplier was been deleted"),
+            @ApiResponse(code = 404, message = "supplier was not found"),
+            @ApiResponse(code = 422, message = "id out of bound, your id is minor to 1")
+    })
     @DeleteMapping(path = "/delete/{idSupplier}")
     public ResponseEntity<Boolean> deleteSupplierById(@PathVariable long idSupplier) {
         supplierService.deleteSupplierById(idSupplier);
         return new ResponseEntity<>(true, HttpStatus.ACCEPTED);
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "supplier was updated"),
+            @ApiResponse(code = 422, message = "supplier is null")
+    })
     @PutMapping(path = "/update")
     public ResponseEntity<Supplier> updateSupplier(@RequestBody Supplier supplier) {
         return new ResponseEntity<>(supplierService.updateSupplier(supplier)
                 , HttpStatus.ACCEPTED);
     }
 
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "supplier was returned"),
+            @ApiResponse(code = 404, message = "supplier was not found")
+    })
     @GetMapping(path = "/get/{idSupplier}")
     public ResponseEntity<Supplier> getSupplierById(@PathVariable long idSupplier) {
         return new ResponseEntity<>(supplierService.getSupplierById(idSupplier)
                 , HttpStatus.OK);
     }
 
+    @ApiResponse(code = 200, message = "all suppliers was been returned")
     @GetMapping(path = "/get/all")
     public ResponseEntity<List<Supplier>> getAllSuppliers() {
         return new ResponseEntity<>(supplierService.getAllSuppliers()
